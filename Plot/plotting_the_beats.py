4162f11d6217f7ce76845aef5f66ca2571cafc78
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2023-12-23 18:15:47 +0530

    Update plotting_the_beats.py

--- Plot/plotting_the_beats.py
+++ Plot/plotting_the_beats.py
@@ -1,21 +1,15 @@
 import librosa 
-import librosa.display
 import pandas as pd
 import numpy as np
 
-path = ''
+path = '' # your path to the wav file goes here
 
-def plot(path):
-    '''
-    use num1 and num2 to scale the plot
-    '''
-    
-    raw_data, screen_rate = librosa.load(path)
-    
-    num1 = 1
-    num2 = 300000
-    
-    new = [abs(i) for i in raw_data[num1:num2]]
-    pd.Series(np.array(new)).plot(figsize=(11,5),lw=0.5)
-    
-plot(path)
+'''
+use num1 and num2 to scale the plot
+'''
+raw_data, screen_rate = librosa.load(path)
+num1 = 1
+num2 = 300000
+new = [abs(i) for i in raw_data[num1:num2]]
+
+pd.Series(np.array(new)).plot(figsize=(11,5),lw=0.5) # specify the line_width as you need
