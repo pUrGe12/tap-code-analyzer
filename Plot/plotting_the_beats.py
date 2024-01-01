c297eb3b64301d98a01e8938101be0467ffd47fd
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-01-01 23:11:01 +0530

    Update plotting_the_beats.py

--- Plot/plotting_the_beats.py
+++ Plot/plotting_the_beats.py
@@ -12,4 +12,4 @@ num1 = 1
 num2 = 300000
 new = [abs(i) for i in raw_data[num1:num2]]
 
-pd.Series(np.array(new)).plot(figsize=(11,5),lw=0.5) # specify the line_width as you need
+pd.Series(np.array(new)).plot(figsize=(11,5),lw=0.5, marker = 'o') # specify the line_width as you need
