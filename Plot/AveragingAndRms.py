4bad8bca3b57ef85373d36ecd405a037ecd5cfa8
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-01-14 12:33:31 +0530

    Update AveragingAndRms.py

--- Plot/AveragingAndRms.py
+++ Plot/AveragingAndRms.py
@@ -3,32 +3,23 @@ import pandas as pd
 import numpy as np
 import math
 
-path = r'C:\Users\hello\Downloads\test.wav' 
-
+path = r'C:\...\test.wav' # path to wav file
 
 raw_data, screen_rate = librosa.load(path)
-num1 = 32000
-num2 = 380000
+num1 = '' # start point
+num2 = '' # end point
     
 initial_list = [abs(i) for i in raw_data[num1:num2]]
 
-#pd.Series(np.array(initial_list)).plot(figsize=(11,5),lw=0.5)
-
 '''
-we have 2 sort of distinctions to make, 1st for counting how much space we give to the tap and 
-2nd for differentitating between taps in the same group with others 
-'''
-
-'''
-idea: try some sort of average function to reduce number of points within some specific ranges
-
-idea2: instead of average try a rms function
+idea1: average function to reduce number of points within some specific ranges
+idea2: RMS function to reduce number of points
 '''
 
 width = 1000
 
 def average_the_plot(width, new_list=[], flag_1=0, flag_2=width+1, averageSum=0, initialisation=0):
-
+    assert type(width) == int, 'enter valid width'  
     while initialisation <= len(initial_list)/width:
         try:
             for i in range(flag_1, flag_2):
@@ -41,10 +32,10 @@ def average_the_plot(width, new_list=[], flag_1=0, flag_2=width+1, averageSum=0,
         except IndexError:
             break
     
-    pd.Series(np.array(new_list)).plot(figsize=(11,5),lw=0.5)
+    pd.Series(np.array(new_list)).plot(figsize=(11,5),lw=0.5) # comment this if you don't want to plot and check
     
 def rms_the_plot(width, new_list=[], flag_1=0, flag_2=width+1, averageSum=0, initialisation=0):
-
+    assert type(width) == int, 'enter valid width'
     while initialisation <= len(initial_list)/width:
         try:
             for i in range(flag_1, flag_2):
@@ -57,6 +48,4 @@ def rms_the_plot(width, new_list=[], flag_1=0, flag_2=width+1, averageSum=0, ini
         except IndexError:
             break
 
-    pd.Series(np.array(new_list)).plot(figsize=(11,5),lw=0.5)
-
-average_the_plot(width)
+    pd.Series(np.array(new_list)).plot(figsize=(11,5),lw=0.5) # comment this if you don't want to plot and check
